cb2939c7cb01e5252aa21afc1c575abc321aeb1a
Maaki <maakintosh404@gmail.com>
2025-05-05 21:30:39 +0900

    update: public.schema: タイムゾーン付与、updatedAtに現在時刻をセット

--- src/lib/db/schema/public.schema.ts
+++ src/lib/db/schema/public.schema.ts
@@ -5,15 +5,21 @@ export const postsTable = pgTable("posts", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   content: text("content").notNull(),
-  createdAt: timestamp("created_at").notNull(),
-  updatedAt: timestamp("updated_at").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .notNull()
+    .defaultNow()
+    .$onUpdateFn(() => new Date()),
 });
 
 export const commentsTable = pgTable("comments", {
   id: serial("id").primaryKey(),
   content: text("content").notNull(),
-  createdAt: timestamp("created_at").notNull(),
-  updatedAt: timestamp("updated_at").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .notNull()
+    .defaultNow()
+    .$onUpdateFn(() => new Date()),
   postId: integer("post_id").references(() => postsTable.id),
 });
 
