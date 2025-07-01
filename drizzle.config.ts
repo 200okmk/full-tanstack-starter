f8e25423f926657a0970970971e577d0d9fb2ff1
Maaki <maakintosh404@gmail.com>
2025-07-01 20:25:10 +0900

    fix: drizzleコンフィグ修正

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default {
   out: "./migrations",
 
   // スキーマ定義ファイルのパス（テーブル定義を含むファイル）
-  schema: "./src/lib/db/schemas/index.ts",
+  schema: "./src/db/schemas/index.ts",
 
   // drizzle-kit push コマンドを実行するときに、出力された SQL ステートメントを実行するかどうかを確認するプロンプトが表示される
   strict: true,
