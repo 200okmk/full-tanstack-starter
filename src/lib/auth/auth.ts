31e94eb5aa6cb2acbebabd80de98c9e71585f024
Maaki <maakintosh404@gmail.com>
2025-12-13 23:31:46 +0900

    refactor(auth): authMiddleware

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -16,6 +16,11 @@ export const auth = betterAuth({
   // process.env.URLを使用（Netlifyランタイムで動的に取得可能）
   baseURL: process.env.URL ?? "http://localhost:3000",
   secret: process.env.BETTER_AUTH_SECRET!,
+  session: {
+    // セッションが使用され、updateAge に達すると、セッションの有効期限は現在時刻に expiresIn 値を加えた値に延長される（https://www.better-auth.com/docs/concepts/session-management#session-expiration）。
+    expiresIn: 60 * 60 * 24 * 30, // デフォルトは7日間
+    updateAge: 60 * 60 * 24, // セッションを更新する頻度
+  },
   database: drizzleAdapter(db, {
     provider: "pg",
     schema: {
@@ -25,15 +30,12 @@ export const auth = betterAuth({
       verification: verificationTokensTable,
     },
   }),
-  session: {
-    // セッションが使用され、updateAge に達すると、セッションの有効期限は現在時刻に expiresIn 値を加えた値に延長される（https://www.better-auth.com/docs/concepts/session-management#session-expiration）。
-    expiresIn: 60 * 60 * 24 * 30, // デフォルトは7日間
-    updateAge: 60 * 60 * 24, // セッションを更新する頻度。
-  },
-  // メールアドレスとパスワード認証。デフォルトでは暗黙的に false。
-  emailAndPassword: {
-    enabled: true,
-  },
+  plugins: [
+    // Netlifyプレビュー環境の動的URL上からもOAuth認証を可能にするために本番のURLへリダイレクトする。
+    oAuthProxy(),
+    // plugins内では必ず最後にこれを配置する（https://www.better-auth.com/docs/integrations/tanstack#usage-tips）。
+    tanstackStartCookies() as unknown as BetterAuthPlugin,
+  ],
   // OAuth認証プロバイダーの設定
   socialProviders: {
     github: {
@@ -45,10 +47,8 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
   },
-  plugins: [
-    // Netlifyプレビュー環境の動的URL上からもOAuth認証を可能にするために本番のURLへリダイレクトする。
-    oAuthProxy(),
-    // （https://www.better-auth.com/docs/integrations/tanstack#usage-tips）
-    tanstackStartCookies() as unknown as BetterAuthPlugin,
-  ],
+  // メールアドレスとパスワード認証。デフォルトでは暗黙的に false。
+  emailAndPassword: {
+    enabled: true,
+  },
 });
---
+++ src/lib/auth/authMiddleware.ts
@@ -0,0 +1,43 @@
+import { redirect } from "@tanstack/react-router";
+import { createMiddleware } from "@tanstack/react-start";
+import {
+  getRequest,
+  setResponseHeader,
+  setResponseStatus,
+} from "@tanstack/react-start/server";
+import { auth } from "~/lib/auth/auth";
+
+/**
+ * 認証済み状態を必要とするServer Functionの実行時に認証チェックを行い、セッション情報をコンテキストに追加するミドルウェア。認証状態でない場合はリダイレクトしてログイン画面に遷移する(https://tanstack.com/start/latest/docs/framework/react/guide/middleware)。
+ */
+export const authMiddleware = createMiddleware().server(async ({ next }) => {
+  const session = await auth.api.getSession({
+    headers: getRequest().headers,
+    query: {
+      // ミドルウェアとして厳格に認証状態を保証するため、Cookieキャッシュからではなくデータベースから直接セッションを取得する。Cookieキャッシュも更新される（https://www.better-auth.com/docs/concepts/session-management#session-caching）。
+      disableCookieCache: true,
+    },
+    returnHeaders: true,
+  });
+
+  // セッション有効期限の延長、Cookieキャッシュの更新などのため、Set-Cookie ヘッダーをTanStack Startのレスポンスヘッダー（クライアント）に転送する。
+  const cookies = session.headers?.getSetCookie();
+  if (cookies?.length) {
+    setResponseHeader("Set-Cookie", cookies);
+  }
+
+  // この認証ミドルウェアの本体部分。
+  if (!session.response?.user) {
+    setResponseStatus(401);
+    // 認証状態でない場合はリダイレクトしてログイン画面に遷移する。
+    throw redirect({
+      to: "/auth/login",
+      // ログイン後に元のページに戻れるようにリダイレクト先を保存。
+      search: {
+        redirect: getRequest().url,
+      },
+    });
+  }
+
+  return next({ context: { sessionUser: session.response?.user } });
+});
