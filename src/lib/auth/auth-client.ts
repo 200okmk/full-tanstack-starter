a6ef144c050043d9b674ac468e37f989c40bf9e4
Maaki <maakintosh404@gmail.com>
2025-12-20 22:40:58 +0900

    feat: ソーシャルログインボタン追加、 GoogleとGithubのSVGコンポーネント追加

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,5 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
-export const authClient = createAuthClient({
+const authClient = createAuthClient({
   baseURL: process.env.URL ?? "http://localhost:3000",
 });
+
+export default authClient;
--- src/routes/(auth-pages)/login.tsx
+++ src/routes/(auth-pages)/login.tsx
@@ -5,7 +5,7 @@ import { z } from "zod";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
-import { authClient } from "~/lib/auth/auth-client";
+import authClient from "~/lib/auth/auth-client";
 
 export const Route = createFileRoute("/(auth-pages)/login")({
   validateSearch: z.object({
