8b3a2ad7d66f39f438ed2ad705838c88374527d4
Maaki <maakintosh404@gmail.com>
2026-01-04 18:22:46 +0900

    chore(build): npmスクリプト変更、vite config修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: {
-    port: 3000,
+  resolve: {
+    tsconfigPaths: true,
   },
   plugins: [
-    tsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
     tanstackStart(),
-    // react's vite plugin must come after start's vite plugin
+    nitro(),
     viteReact({
       // https://react.dev/learn/react-compiler
       babel: {
@@ -29,4 +26,9 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
 });
